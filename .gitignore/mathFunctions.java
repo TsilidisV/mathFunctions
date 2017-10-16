8abff6734d4ba4fac6414751e15c11feab7d8446
blueRoseXIV <32766153+blueRoseXIV@users.noreply.github.com>
2017-10-16 12:31:48 +0300

    Add files via upload

---
+++ .gitignore/mathFunctions.java
@@ -0,0 +1,35 @@
+
+/**
+ * calculates factorial of a given natural number
+ * 
+ * @blueRose
+ */
+
+
+public class mathFunctions
+{
+
+  public static int factorial(int a)
+  {
+     int c;
+     if (a==0)
+         return 0;
+     else 
+         c=1;
+         for (int i = 1; i<=a; i=i+1)
+         c=c*i; 
+         return c;
+  }
+ 
+  public static int combinations(int a, int b)
+  {
+     if (a<b)
+         return 0;
+     else if (a==b)
+         return 1;
+     else 
+        return factorial(a)/(factorial(b)*factorial(a-b)) ;
+ 
+  }
+
+}
